0b434bf1f62f217181852664f65ed79eb786a1dd
francescocartelli <60184893+francescocartelli@users.noreply.github.com>
2022-05-23 10:02:23 +0200

    recalibration.py updated

--- PatternLib/recalibration.py
+++ PatternLib/recalibration.py
@@ -4,15 +4,19 @@ from PatternLib.pipeline import Pipe
 
 
 class Recalibration(Pipe):
-    def __init__(self, prior=None, norm_coeff=10**-5):
+    def __init__(self, prior=0.5, norm_coeff=10**-5):
         self.log_reg = LogisticRegression(norm_coeff=norm_coeff, prior=prior)
 
     def fit(self, x, y):
+        x = x.reshape(x.size, 1)
         self.log_reg.fit(x, y)
 
     def transform(self, x):
-        w, b = self.log_reg.w, self.log_reg.b
-        return np.dot(w, x + b)
+        x = x.reshape(x.size, 1)
+
+        w, b_, prior = self.log_reg.w, self.log_reg.b, self.log_reg.prior
+        b = b_ - (np.log(prior/(1 - prior)))
+        return np.dot(w, x.T) + b
 
     def fit_transform(self, x, y):
         self.fit(x, y)
